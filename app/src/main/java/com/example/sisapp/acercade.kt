b332f3a1df427a15c10c221284fb3606248f05f1
isabelbenitesrodriguez <133533683+isabelbenitesrodriguez@users.noreply.github.com>
2023-11-10 19:02:58 -0500

    Initial commit

---
+++ app/src/main/java/com/example/sisapp/acercade.kt
@@ -0,0 +1,11 @@
+package com.example.sisapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class acercade : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_acercade)
+    }
+}
---
+++ app/src/main/java/com/example/sisapp/serviciotriaje.kt
@@ -0,0 +1,11 @@
+package com.example.sisapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class serviciotriaje : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_serviciotriaje)
+    }
+}
